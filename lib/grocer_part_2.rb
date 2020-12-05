c0d951b93c3f91db82392bc9ef6a6bb0a8da8e73
Alexander S <alx.sherwin@gmail.com>
2020-12-05 23:38:57 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,18 +1,59 @@
+require "pry"
 require_relative './part_1_solution.rb'
 
-def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+def find_item_by_name_in_collection(name, collection)
+  index = 0
+
+  collection.each do |grocery_item|
+    return grocery_item if grocery_item[:item] === name 
+    index += 1
+  end
+
+  nil
 end
 
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+def apply_coupons(cart, coupons)
+   counter = 0 
+   while counter < coupons.length 
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/ COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+
+   if cart_item && cart_item[:count] >= coupons[counter][:num]
+     if cart_item_with_coupon 
+       cart_item_with_coupon[:count] += coupons[counter][:num]
+       cart_item[:count] -= coupons[counter][:num]
+     else
+       cart_item_with_coupon = {
+         :item => couponed_item_name,
+         :price => (coupons[counter][:cost] / coupons[counter][:num]),
+         :count => coupons[counter][:num],
+         :clearance => cart_item[:clearance]
+       }
+       cart << cart_item_with_coupon
+       cart_item[:count] -= coupons[counter][:num]
+    end
+  end
+   counter += 1 
+  end
+  cart
+end
+
+def apply_clearance(cart) 
+  
+   cart.each do |item, details|
+    if cart[item][:clearance] == true
+      cart[item][:price] = (cart[item][:price]*0.20 - total)
+    end
+  end
+  cart
+ 
 end
 
 def checkout(cart, coupons)
+  
+  
+  
   # Consult README for inputs and outputs
   #
   # This method should call
