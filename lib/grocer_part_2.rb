eb255dd8e996ac0e382bdec9cfdbcd384d472fa1
Alexander S <alx.sherwin@gmail.com>
2020-12-06 00:24:43 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,7 +3,6 @@ require_relative './part_1_solution.rb'
 
 def find_item_by_name_in_collection(name, collection)
   index = 0
-
   collection.each do |grocery_item|
     return grocery_item if grocery_item[:item] === name 
     index += 1
